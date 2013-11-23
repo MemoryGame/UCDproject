4ae11b5aa4fcf901f66f716d969970256f6a85f6
Kate Hanahoe <kate.hanahoe@ucdconnect.ie>
2013-11-23 14:50:25 +0000

    More tidying up

--- MemoryGame/src/com/example/memorygame/PlayGame.java
+++ MemoryGame/src/com/example/memorygame/PlayGame.java
@@ -35,8 +35,8 @@ public class PlayGame extends SherlockActivity implements OnClickListener {
 	ArrayList<Integer> userGuess = new ArrayList<Integer>();
 	
 	// The buttons will have different background colours
-	final int[] coloursOn = new int[]{R.drawable.blue_button_on, R.drawable.orange_button_on, R.drawable.yellow_button_on, R.drawable.purple_button_on, R.drawable.green_button_on, R.drawable.red_button_on, R.drawable.black_button_on, R.drawable.pink_button_on};
-	final int[] coloursOff = new int[]{R.drawable.blue_button_off, R.drawable.orange_button_off, R.drawable.yellow_button_off, R.drawable.purple_button_off, R.drawable.green_button_off, R.drawable.red_button_off, R.drawable.black_button_off, R.drawable.pink_button_off};
+	final int[] buttonsOn = new int[]{R.drawable.blue_button_on, R.drawable.orange_button_on, R.drawable.yellow_button_on, R.drawable.purple_button_on, R.drawable.green_button_on, R.drawable.red_button_on, R.drawable.black_button_on, R.drawable.pink_button_on};
+	final int[] buttonsOff = new int[]{R.drawable.blue_button_off, R.drawable.orange_button_off, R.drawable.yellow_button_off, R.drawable.purple_button_off, R.drawable.green_button_off, R.drawable.red_button_off, R.drawable.black_button_off, R.drawable.pink_button_off};
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -76,131 +76,17 @@ public class PlayGame extends SherlockActivity implements OnClickListener {
 			}
 		}
 		
-		generateLayout(numButtons, coloursOff);
+		generateLayout(numButtons, buttonsOff);
 		pattern = newPattern(sequenceLength, numButtons);
-		playSequence();
+		
+		Timer myTimer = new Timer();
+		Handler myHandler = new Handler();
+		
+		long delay = playSequence(myTimer, myHandler, pattern, timeBetweenChangesMs, buttonsOn, buttonsOff);
+		setOnClickListeners(this, myTimer, myHandler, numButtons, delay);
 		
       	}
 
-	private ArrayList<Integer> newPattern(int length, int n) {
-		ArrayList<Integer> newPattern = new ArrayList<Integer>();
-		//random number generator
-		Random r = new Random();
-		for(int i =0; i<length;i++){
-			int x = r.nextInt(n);
-    		newPattern.add(x);
-    	}
-		return newPattern;
-	}
-
-	private void playSequence() {
-		// display this new number pattern to the user using buttons
-		// i.e. for each number in the pattern, select the button that has a matching id
-		// temporarily change the text of this button for a few seconds
-		// then change it back again and move onto the next button		
-		final Timer t = new Timer();
-		long delay = 1;
-		final Handler h = new Handler();
-		for(int i = 0; i < sequenceLength;i++){
-			final Button b = ((Button)findViewById(pattern.get(i)));
-			final int bNum = pattern.get(i);
-			delay += timeBetweenChangesMs;
-			t.schedule(new TimerTask(){
-				public void run(){
-					h.post(new Runnable(){
-						public void run(){
-				        	b.setBackgroundResource(coloursOn[bNum]);
-				        }
-				    });
-				}
-			}, delay);
-			delay += timeBetweenChangesMs;
-			t.schedule(new TimerTask(){
-				public void run(){
-					h.post(new Runnable(){
-						public void run(){
-				        	b.setBackgroundResource(coloursOff[bNum]);
-				        }
-				    });
-				}
-			}, (delay));
-    	}
-		
-		delay += timeBetweenChangesMs;
-		t.schedule(new TimerTask(){
-			public void run(){
-				h.post(new Runnable(){
-					public void run(){
-						for(int i =0; i<numButtons;i++){
-							Button b = ((Button)findViewById(i));
-							b.setOnClickListener(PlayGame.this);
-				    	}
-				    }
-				});
-			}
-		}, delay);
-	}
-
-	private void generateLayout(int numBtns, final int[] colours) {
-		// start generating the layout 
-		LinearLayout mainLayout = ((LinearLayout)findViewById(R.id.mainLayout));
-		
-	    /*
-	     * The layout will be made up of rows of two buttons side-by-side
-	     * so if there are 6 buttons then there will be 3 rows
-	     * if there are 5 buttons there will also be 3 rows, the last row will only have one button
-	     * */
-		
-		// based on the number of buttons figure out how many rows are needed
-		int numRows = numBtns/2;
-		// if there is an odd number of buttons we need an extra row and set odd == true
-		boolean odd = false;
-		if (numBtns%2 != 0){
-			numRows++;
-			odd=true;
-		}
-		
-		// each "row" talked about above is actually a linearLayout
-		LinearLayout[] rows = new LinearLayout[numRows];
-		
-		// create a set of default linearLayout parameters to be given to every linearLayout 
-		LayoutParams llParams = new LayoutParams(LayoutParams.MATCH_PARENT,LayoutParams.WRAP_CONTENT);
-		LayoutParams bParams = new LayoutParams(LayoutParams.WRAP_CONTENT,LayoutParams.WRAP_CONTENT);
-
-		// add the buttons to each linearLayout row
-		// each button created will be given a unique number starting at 0
-		int buttonNum = 0;
-		for(int i=0; i<numRows; i++){	
-			rows[i] = new LinearLayout(this);
-			// set parameters for the current linearLayout row
-			rows[i].setOrientation(LinearLayout.HORIZONTAL);
-			rows[i].setLayoutParams(llParams);	
-			// if we've reached the last linearLayout row and there is an odd number of buttons, add only one button
-			if( i==(numRows-1) && odd){
-				Button btn = new Button(this);
-	        	btn.setLayoutParams(bParams);
-	        	btn.setBackgroundResource(colours[buttonNum]);
-	        	btn.setId(buttonNum);
-	        	rows[i].addView(btn);
-	        	buttonNum++;
-			} 
-			// otherwise add two buttons to the current linearLayout row
-			else {
-				Button[] rowButtons = new Button[2];
-				for(Button btn: rowButtons){
-				    btn = new Button(this);
-		        	btn.setLayoutParams(bParams);
-		        	btn.setBackgroundResource(colours[buttonNum]);
-		        	btn.setId(buttonNum);
-		        	rows[i].addView(btn);
-		        	buttonNum++;
-				}
-			} 	
-			mainLayout.addView(rows[i]);
-		}
-		// finish generating the layout 
-		
-	}
 
 	@Override
 	public void onClick(View v) {
@@ -284,66 +170,122 @@ public class PlayGame extends SherlockActivity implements OnClickListener {
         AlertDialog alerta = builder.create();
         alerta.show();
 	}
-		
+
+	
+	private void generateLayout(int numBtns, final int[] colours) {
+		// start generating the layout 
+		LinearLayout mainLayout = ((LinearLayout)findViewById(R.id.mainLayout));	
+	    /*
+	     * The layout will be made up of rows of two buttons side-by-side
+	     * so if there are 6 buttons then there will be 3 rows
+	     * if there are 5 buttons there will also be 3 rows, the last row will only have one button
+	     * */
+		// based on the number of buttons figure out how many rows are needed
+		int numRows = numBtns/2;
+		// if there is an odd number of buttons we need an extra row and set odd == true
+		boolean odd = false;
+		if (numBtns%2 != 0){
+			numRows++;
+			odd=true;
+		}
+		// each "row" talked about above is actually a linearLayout
+		LinearLayout[] rows = new LinearLayout[numRows];
+		// create a set of default linearLayout parameters to be given to every linearLayout 
+		LayoutParams llParams = new LayoutParams(LayoutParams.MATCH_PARENT,LayoutParams.WRAP_CONTENT);
+		LayoutParams bParams = new LayoutParams(LayoutParams.WRAP_CONTENT,LayoutParams.WRAP_CONTENT);
+		// add the buttons to each linearLayout row
+		// each button created will be given a unique number starting at 0
+		int buttonNum = 0;
+		for(int i=0; i<numRows; i++){	
+			rows[i] = new LinearLayout(this);
+			// set parameters for the current linearLayout row
+			rows[i].setOrientation(LinearLayout.HORIZONTAL);
+			rows[i].setLayoutParams(llParams);	
+			// if we've reached the last linearLayout row and there is an odd number of buttons, add only one button
+			if( i==(numRows-1) && odd){
+				Button btn = new Button(this);
+	        	btn.setLayoutParams(bParams);
+	        	btn.setBackgroundResource(colours[buttonNum]);
+	        	btn.setId(buttonNum);
+	        	rows[i].addView(btn);
+	        	buttonNum++;
+			} 
+			// otherwise add two buttons to the current linearLayout row
+			else {
+				Button[] rowButtons = new Button[2];
+				for(Button btn: rowButtons){
+				    btn = new Button(this);
+		        	btn.setLayoutParams(bParams);
+		        	btn.setBackgroundResource(colours[buttonNum]);
+		        	btn.setId(buttonNum);
+		        	rows[i].addView(btn);
+		        	buttonNum++;
+				}
+			} 	
+			mainLayout.addView(rows[i]);
+		}
+		// finish generating the layout 
+	}
+
+	private ArrayList<Integer> newPattern(int length, int n) {
+		ArrayList<Integer> newPattern = new ArrayList<Integer>();
+		//random number generator
+		Random r = new Random();
+		for(int i =0; i<length;i++){
+			int x = r.nextInt(n);
+    		newPattern.add(x);
+    	}
+		return newPattern;
+	}
+
+	private long playSequence(Timer t, final Handler h, ArrayList<Integer> newPattern, long milliseconds, final int[] on, final int[] off) {
+		// display this new number pattern to the user using buttons
+		// i.e. for each number in the pattern, select the button that has a matching id
+		// temporarily change the text of this button for a few seconds
+		// then change it back again and move onto the next button		
+		long delay = 1;
+		for(int i = 0; i < newPattern.size();i++){
+			final Button b = ((Button)findViewById(newPattern.get(i)));
+			final int bNum = newPattern.get(i);
+			delay += milliseconds;
+			t.schedule(new TimerTask(){
+				public void run(){
+					h.post(new Runnable(){
+						public void run(){
+				        	b.setBackgroundResource(on[bNum]);
+				        }
+				    });
+				}
+			}, delay);
+			delay += milliseconds;
+			t.schedule(new TimerTask(){
+				public void run(){
+					h.post(new Runnable(){
+						public void run(){
+				        	b.setBackgroundResource(off[bNum]);
+				        }
+				    });
+				}
+			}, (delay));
+    	}
+		delay += milliseconds;
+		return delay;
+	}
+	
+	private void setOnClickListeners(final PlayGame p, Timer t, final Handler h, final int n, long d) {
+		t.schedule(new TimerTask(){
+			public void run(){
+				h.post(new Runnable(){
+					public void run(){
+						for(int i =0; i<n;i++){
+							Button b = ((Button)findViewById(i));
+							b.setOnClickListener(p);
+				    	}
+				    }
+				});
+			}
+		}, d);
+	}
+	
 
 }
-
-
-
-//package com.example.memorygame;
-//
-//import android.content.Intent;
-//import android.os.Bundle;
-//
-//import com.actionbarsherlock.app.SherlockActivity;
-//import com.actionbarsherlock.view.Menu;
-//import com.actionbarsherlock.view.MenuInflater;
-//import com.actionbarsherlock.view.MenuItem;
-//
-//public class PlayGame extends SherlockActivity {
-//
-//	@Override
-//	protected void onCreate(Bundle savedInstanceState) {
-//		super.onCreate(savedInstanceState);
-//		setContentView(R.layout.activity_play_game);
-//
-//	}
-//
-//	@Override
-//	public boolean onCreateOptionsMenu(Menu menu) {
-//		new MenuInflater(this).inflate(R.menu.main_menu, menu);
-//
-//		return (super.onCreateOptionsMenu(menu));
-//	}
-//
-//	@Override
-//	public boolean onOptionsItemSelected(MenuItem item) {
-//		switch (item.getItemId()) {
-//		case android.R.id.home:
-//
-//		case R.id.action_settings:
-//			abOptions();
-//			return (true);
-//
-//		case R.id.action_info:
-//			abInfo();
-//			return (true);
-//
-//		}
-//		return (super.onOptionsItemSelected(item));
-//	}
-//
-//	// Options activity launched from the actionBar
-//	public void abOptions() {
-//
-//		startActivity(new Intent(this, Options.class));
-//
-//	}
-//
-//	public void abInfo() {
-//
-//		startActivity(new Intent(this, About.class));
-//
-//	}
-//
-//}
