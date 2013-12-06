4aea518ad24f4ee6738f7af7e2167a50558a69a4
Eoin O'Connor <daedalusgroup@gmail.com>
2013-12-06 13:40:57 +0000

    Adjusted navigation and input type for InsertScores

--- MemoryGame/src/com/example/memorygame/InsertScores.java
+++ MemoryGame/src/com/example/memorygame/InsertScores.java
@@ -5,6 +5,7 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -63,6 +64,13 @@ public class InsertScores extends SherlockActivity {
 				alert.show();
 			}
 		}
+	}
+	public boolean onKeyDown(int keyCode, KeyEvent event) {
+		if (keyCode == KeyEvent.KEYCODE_BACK) {
+			finish();
+			startActivity(new Intent(this,MainMenu.class));
+		}
+		return super.onKeyDown(keyCode, event);
 	}
 
 }
